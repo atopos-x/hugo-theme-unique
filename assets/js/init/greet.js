fa58a45ccec4673a79fb3db8580ee91eaed2b8ef
Atopos <qs154152@163,com>
2024-11-18 19:57:21 +0800

    del

--- assets/js/init/greet.js
+++
@@ -1,9 +0,0 @@
-export default function greet () {
-	let year = new Date().getFullYear();
-	console.log(
-		`---------------------
-		\n\tHello Jack 😎, ${year} !\n
-		---------------------`
-	);
-	document.querySelector('#info-date').innerHTML = year;
-}
